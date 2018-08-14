4a624137c32b578cc8a1c6d245ba15c3cf95f697
ymjia <ymjia90@163.com>
2018-08-14 17:45:57 +0800

    fix bug

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -100,7 +100,7 @@
 
 (global-set-key [remap query-replace] 'anzu-query-replace)
 (global-set-key [remap query-replace-regexp] 'anzu-query-replace-regexp)
-)
+
 
 
 ;==================customizations=====================
